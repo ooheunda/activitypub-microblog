033274a3d8a59dab5470dccc5f9cad06b98e6869
ooheunda <ooheunda@gmail.com>
2025-07-16 00:51:46 +0900

    Add POST /setup

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ import { federation } from '@fedify/fedify/x/hono';
 import { getLogger } from '@logtape/logtape';
 import fedi from './federation.ts';
 import { Layout, SetupForm } from './views.tsx';
+import db from './db.ts';
+import type { User } from './schema.ts';
 
 const logger = getLogger('microblog');
 
@@ -10,12 +12,33 @@ const app = new Hono();
 app.use(federation(fedi, () => undefined));
 
 app.get('/', (c) => c.text('Hello, Fedify!'));
-app.get('/setup', (c) =>
-  c.html(
+
+app.get('/setup', (c) => {
+  // 계정이 이미 있는지 검사
+  const user = db.prepare<unknown[], User>('SELECT * FROM users LIMIT 1').get();
+  if (user != null) return c.redirect('/');
+
+  return c.html(
     <Layout>
       <SetupForm />
     </Layout>
-  )
-);
+  );
+});
+
+app.post('/setup', async (c) => {
+  // 계정이 이미 있는지 검사
+  const user = db.prepare<unknown[], User>('SELECT * FROM users LIMIT 1').get();
+  if (user != null) return c.redirect('/');
+
+  const form = await c.req.formData();
+  const username = form.get('username');
+
+  if (typeof username !== 'string' || !username.match(/^[a-z0-9_-]{1,50}$/)) {
+    return c.redirect('/setup');
+  }
+
+  db.prepare('INSERT INTO users (username) VALUES (?)').run(username);
+  return c.redirect('/');
+});
 
 export default app;
